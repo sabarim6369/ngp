90f3cedaa52d05c2cd2f681fa2cd59dbdfe1942f
sabarim6369 <sabarim6369@gmail.com>
2025-02-08 14:27:26 +0530

    first commit1

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind!</h1>
+    </div>
+  );
+}
